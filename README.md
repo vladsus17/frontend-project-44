2fefcf79c61e8901d4ad28085109ac7133200710
Vladislav Susickis <susickis@hotmail.com>
2024-10-01 18:23:27 +0100

    add brain-prime

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ brain-calc:  https://asciinema.org/a/1NcJq38m60ENVbd7kbpmh1Vgk
 brain-gcd:  https://asciinema.org/a/LDDvEsHZrplYxjN4Nwl7oY7i0
 
 brain-progression:  https://asciinema.org/a/8mR0fVEErYgJsSGLiYlSbNsSI
+
+brain-prime: https://asciinema.org/a/fvojhjFou187vSe8g5LDK3JzA
---
+++ bin/brain-prime.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { greetUser } from '../src/cli.js';
+import { generateRandomNumber } from '../src/index.js';
+
+greetUser();
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const brainPrime = () => {
+
+    let correctAnswersCount = 0;
+
+    const isPrime = (number) => {
+        let divisor = 2;
+        if (number <= 1) return false;
+        while (divisor * divisor <= number) {
+            if (number % divisor === 0) return false;
+                divisor += 1; 
+        } return true;
+    };
+
+    while (correctAnswersCount < 3) {
+        const randomNum = generateRandomNumber();
+        console.log(`Question: ${randomNum} `);
+        const userAnswer = readlineSync.question('Your answer: ')
+        const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+
+        if (userAnswer === correctAnswer) {
+        console.log('Correct!');
+        correctAnswersCount += 1;
+        } else {
+        console.log(`Wrong answer! The correct answer was ${correctAnswer}.`);
+        console.log('Let\'s try again!');
+        return;
+    }   
+    }
+    console.log('Congratulations, you won!');
+};
+
+brainPrime();
+
