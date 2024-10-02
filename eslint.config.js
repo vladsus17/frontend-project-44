5b3f911aa01343c6b37f68658a4b28ab777bde44
Vladislav Susickis <susickis@hotmail.com>
2024-10-02 20:10:30 +0100

    modified eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        Math: 'readonly',
+        parseFloat: 'readonly',
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
